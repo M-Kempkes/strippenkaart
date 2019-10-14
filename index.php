574bbffa52dbdd7b4fca26a04d74589d802f14ef
Marco Kempkes <2088979@talnet.nl>
2019-10-14 10:06:27 +0200

    updated code (Twitter API not operational)

--- index.php
+++ index.php
@@ -10,22 +10,24 @@
 	<meta name="keywords" content="">
 	<meta name="author" content="Marco Kempkes">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href=".\main.css">
+	<link rel="stylesheet" type="text/css" href=".\css\main.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<script defer src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 	<script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script defer src=".\main.js"></script>
-	<script defer src=".\instaFeedAPI.js"></script></head>
+	<script defer src=".\js\main.js"></script>
+	<script defer src=".\js\instaPics.js"></script>
+	<script defer src=".\js\twitterAPI.js"></script>
+</head>
 <body>
 	<div class="container">
 		<header>
 		</header>
 
 		<div class="row">
-			<div id="instagramFeed" class="col-5 h-100"></div>
-			<div class="col-2"></div>
-			<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fkpn&colorscheme=light&show_faces=true&border_color&stream=true&header=true&width=400" scrolling="yes" style="border:none; background: white; " allowtransparency="true" frameborder="0" class="col-5 h-100" id="fbFeed"></iframe>
+			<div id="instagramFeed" class="col-6 h-100"></div>
+			<div class=""></div>
+			<div id="twitterFeed" class="col-6 h-100"></div>
 		</div>
 
 		<footer>
---
+++ js/instaPics.js
@@ -0,0 +1,38 @@
+//Code by: Marco Kempkes
+//puts latest instagram posts in div called instagramFeed
+let instaAuthToken = '467645643.5e76c80.b4a671cba4594d489d51d2a0fe48747c';//insert access token from instagram developer panel
+let instaURL = "https://api.instagram.com/v1/users/self/media/recent/?access_token="+instaAuthToken;
+let instaFeed = ''; 
+let instaPostAmount = 0;//Amount of posts displayed if 0 all posts will be displayed
+$( document ).ready(function($) {
+   	$.ajax({
+        url: instaURL,
+        dataType: "json",
+        cache: false,
+        success: function (response) {
+			var posts = [];
+			if(instaPostAmount === 0){
+				instaPostAmount = response.data.length;
+			}
+			console.log(response.data);
+			
+			for (var i = 0; i <= instaPostAmount-1; i++) {	
+				//create embed with caption
+				posts[i] = response.data[i].images.standard_resolution.url;
+
+				//create an iframe with the post link and sets width and height based off of the image height and width
+				instaFeed += '<img class="col-sm-4" src="'+posts[i]+'"></img>';
+
+			}
+
+			//$('#instagramFeed').css('width', response.data[0].images.standard_resolution.width/divisionAmount+20);
+			$('#instagramFeed').html(instaFeed);
+
+        },
+        error: function () {
+            $("#instagramFeed").html("<p>No response from ajax call</p>");
+        }
+
+	});
+});
+//width="'+(response.data[i].images.standard_resolution.width/divisionAmount)+'" 
